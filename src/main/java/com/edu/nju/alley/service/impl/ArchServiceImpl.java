458393f0266e39c8e64369ffc2a41829e412f34a
zzh <2830793096@qq.com>
2021-08-14 00:22:28 +0800

    feat:增加了ArchSerbiceImpl的data引用

--- src/main/java/com/edu/nju/alley/service/impl/ArchServiceImpl.java
+++ src/main/java/com/edu/nju/alley/service/impl/ArchServiceImpl.java
@@ -1,5 +1,6 @@
 package com.edu.nju.alley.service.impl;
 
+import com.edu.nju.alley.dao.ArchDataService;
 import com.edu.nju.alley.dto.ArchCommentDTO;
 import com.edu.nju.alley.service.ArchService;
 import com.edu.nju.alley.vo.ArchVO;
@@ -8,16 +9,26 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class ArchServiceImpl implements ArchService {
+
+    private final ArchDataService archDataService;
+
+    public ArchServiceImpl(ArchDataService archDataService){
+        this.archDataService=archDataService;
+    }
+
+    //查看建筑信息
     @Override
     public ArchVO view(Integer archId) {
         return null;
     }
 
+    //评论建筑
     @Override
     public CommentVO comment(ArchCommentDTO archCommentDTO) {
         return null;
     }
 
+    //对建筑评分
     @Override
     public Object mark(Integer archId, Integer score) {
         return null;
